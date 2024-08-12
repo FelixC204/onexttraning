5cdff2cdcc69cc768b979cf7eb938de91ec8a27a
khanh <nguyenquockhanhmusic@gmail.com>
2024-08-12 14:38:21 +0700

    install

---
+++ install.php
@@ -0,0 +1,8 @@
+<?php
+// Bước 1 : Tải php https://windows.php.net/downloads/releases/php-8.3.10-nts-Win32-vs16-x64.zip
+// Bước 2 : Giải nén file vừa tải về
+// Bước 3 : Copy thư mục php-8.3.10-nts-Win32-vs16-x64 vào ổ C
+// Bước 4 : Mở cmd và chạy lệnh php -v
+phpinfo();
+phpversion();
+?>
