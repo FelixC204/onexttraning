f39fb27b213d521bd8e6cd8470a70dccbe6ebb30
khanh <nguyenquockhanhmusic@gmail.com>
2024-08-12 14:41:44 +0700

    syntax

---
+++ syntax.php
@@ -0,0 +1,22 @@
+<?php
+// Cú pháp của PHP
+// Cú pháp PHP bắt đầu bằng dấu <?php và kết thúc bằng dấu ? >
+// Các câu lệnh PHP kết thúc bằng dấu chấm phẩy (;)
+// Các câu lệnh PHP không phân biệt chữ hoa và chữ thường
+
+?>
+
+<!-- php có thể viết trong mã html -->
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+    <?php
+        echo "Hello World!";
+    ?>
+ </body>
+ </html>
